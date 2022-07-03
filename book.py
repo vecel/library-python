bed178d2adc9a4e930308eb18e4e2c1a57f5a0bb
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-07-03 21:00:58 +0200

    Add book class fields

---
+++ book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, id: int, title: str, author: str, status: str, rating: int, country: str):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.status = status
+        self.rating = rating
+        self.country = country
