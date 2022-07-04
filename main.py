3d5abcb6121eb7b7332bd0beecbe95431aed349d
Mateusz Karandys <mateusz.karandys@gmail.com>
2022-07-04 17:53:07 +0200

    Change load function to operate on specified library object and .txt file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from library import Book, Library
 
-LIBRARY_FILE_NAME = 'library.txt'
-
-library = Library()
-
-def load():
-    '''Open library file and load its content'''
+def load(library: Library, filename: str):
+    '''Load contents of library database from txt file to library object
+    
+    Arguments:
+    library - Library object to be loaded
+    filename - txt file that contains library data'''
     
     def create_book(properties: list) -> Book :
         id      = int(properties[0])
@@ -17,17 +17,20 @@ def load():
         return Book(id, title, author, status, rating, country)
 
 
-
-    lib = open(LIBRARY_FILE_NAME)
+    try:
+        lib_DB = open(filename)
+    except:
+        print(f'Cannot find {filename}, Library not loaded.')
+        return
 
     # skip first line
-    lib.readline()
+    lib_DB.readline()
 
-    for record in lib.readlines():
+    for record in lib_DB.readlines():
         b = create_book(record.split(';'))
         library.add(b)
         
-    lib.close()
+    lib_DB.close()
 
 def run():
     '''Operate on library data'''
@@ -39,5 +42,10 @@ def close():
     This function clears and rewrites library file'''
     pass
 
-load()
-library.print_catalog()
+
+LIBRARY_FILE_NAME = 'library.txt'
+
+library = Library()
+
+load(library, LIBRARY_FILE_NAME)
+library.display_catalog()
